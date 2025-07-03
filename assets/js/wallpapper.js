d40060bb8040bafc8763a0dda4cf2ba880aa6ba4
Script'Enjøyer <apexdevweb@gmail.com>
2025-07-03 10:58:22 +0200

    stream test

--- assets/js/wallpapper.js
+++ assets/js/wallpapper.js
@@ -1,6 +1,7 @@
 const papperCtnr = document.querySelectorAll(
   ".vid__article--primary-container"
 );
+
 const papperImg = document.querySelectorAll(".vid__article--img");
 papperCtnr.forEach((imgCover, index) => {
   const coveridx = papperImg[index];
