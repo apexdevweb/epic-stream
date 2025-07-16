35fa6ba557c92e4bb369236c2afa5862c0dd90f9
Script'Enjøyer <apexdevweb@gmail.com>
2025-07-16 12:16:14 +0200

    stream test

--- backend/security/adminSecure.php
+++ backend/security/adminSecure.php
@@ -2,7 +2,7 @@
 session_start();
 if (!isset($_SESSION['adm_Auth']) && !isset($_SESSION['admin_data']['adm_name'])) {
     $_SESSION = [];
-    session_destroy();
+    session_unset();
     header('Location: ../../index.php');
     exit;
 }
